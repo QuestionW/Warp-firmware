83bda7aac16b4d74729e0adb5dfb824fd12d6797
QuestionW <zqw876563151@hotmail.com>
2020-11-18 15:49:45 +0000

    keep consistency

--- src/boot/ksdk1.1.0/devINA219.h
+++ src/boot/ksdk1.1.0/devINA219.h
@@ -1,4 +1,4 @@
-void initINA219current(const uint8_t i2cAddress, WarpI2CDeviceState volatile * deviceStatePointer);
+void initINA219(const uint8_t i2cAddress, WarpI2CDeviceState volatile * deviceStatePointer);
 WarpStatus readSensorRegisterINA219(uint8_t deviceRegister);
 WarpStatus      readSensorSignalINA219(WarpTypeMask signal,
                                         WarpSignalPrecision precision,
