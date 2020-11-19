3844ea35a1ae7befc419f72db3e985ba62819148
QuestionW <zqw876563151@hotmail.com>
2020-11-19 16:53:21 +0000

    Antoher bug fixed, defining a device

--- src/boot/ksdk1.1.0/devINA219.h
+++ src/boot/ksdk1.1.0/devINA219.h
@@ -1,3 +1,6 @@
+#ifndef WARP_BUILD_ENABLE_DEVINA219
+#define WARP_BUILD_ENABLE_DEVINA219
+#endif
 void initINA219(const uint8_t i2cAddress, WarpI2CDeviceState volatile * deviceStatePointer);
 WarpStatus readSensorRegisterINA219(uint8_t deviceRegister);
 WarpStatus      readSensorSignalINA219(WarpTypeMask signal,
