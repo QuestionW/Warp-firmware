3edfb25f98becf66068c032093434152a233053f
QuestionW <zqw876563151@hotmail.com>
2020-11-19 15:24:47 +0000

    new method for calculating current

--- src/boot/ksdk1.1.0/devINA219.h
+++ src/boot/ksdk1.1.0/devINA219.h
@@ -2,10 +2,8 @@
 #define WARP_BUILD_ENABLE_DEVINA219
 #endif
 
-void initINA219(const uint8_t i2cAddress, WarpI2CDeviceState volatile * deviceStatePointer);
-WarpStatus readSensorRegisterINA219(uint8_t deviceRegister);
-WarpStatus      readSensorSignalINA219(WarpTypeMask signal,
-                                        WarpSignalPrecision precision,
-                                        WarpSignalAccuracy accuracy,
-                                        WarpSignalReliability reliability,
-                                        WarpSignalNoise noise);
+void		initINA219(const uint8_t i2cAddress, WarpI2CDeviceState volatile *  deviceStatePointer);
+WarpStatus	readSensorRegisterINA219(int numberOfBytes);
+WarpStatus	writeSensorRegisterINA219(uint8_t deviceRegister,
+					uint8_t payloadBtye, uint16_t menuI2cPullupValue);
+void		printSensorDataINA219();
